71253d80de09cd822a308a4ff4100914e9651947
justdk06 <justdk06@gmail.com>
2025-06-26 15:06:44 +0530

    commits all

--- React-Blog/React-blog/src/App.jsx
+++ React-Blog/React-blog/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="text-3xl font-bold text-blue-500">
+    <div className="text-3xl font-bold border-2 border-green-600 bg-red-700">
       Tailwind is working!
     </div>
   );
