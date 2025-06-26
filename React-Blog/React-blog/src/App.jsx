bd7aeb2bd54302bcddb83471ba0452cf8060b6c2
justdk06 <justdk06@gmail.com>
2025-06-26 16:31:27 +0530

    feat: ceate projects add fivecon use tailwids css stylings integration

--- React-Blog/React-blog/src/App.jsx
+++ React-Blog/React-blog/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="text-3xl font-bold border-2 border-green-600 bg-red-700">
+    <div className="text-3xl font-bold border-5 border-green-600 bg-red-700">
       Tailwind is working!
     </div>
   );
