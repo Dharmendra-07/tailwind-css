346ccb7861212fa6b7c4732a67fd3bcafbe1ba6b
justdk06 <justdk06@gmail.com>
2025-06-26 09:24:16 +0530

    feat: implement dark mode toggle and enhance CSS utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["*"],
-//   theme: {
-//     extend: {},
-//     container: {
-//       center: true
-//     }
-//   },
-//   plugins: []
-// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        '21': '6rem',
+        '22': '3rem',
+        '13': '13px',
+        'px-8': '8px',
+        'px-100': '100px',
+      }
+    },
+    container: {
+      center: true
+    }
+  },
+  plugins: []
+};
